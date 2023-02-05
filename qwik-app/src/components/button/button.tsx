dd80843cb592c50f52b3366d44f4ee174d2da1cb
Domi <dominik.reinert.merzig@gmail.com>
2023-02-05 11:38:54 +0100

    add links and icons to the overview

--- qwik-app/src/components/button/button.tsx
+++ qwik-app/src/components/button/button.tsx
@@ -1,22 +1,25 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import { Link, LinkProps } from '@builder.io/qwik-city';
+import { JSX } from '@builder.io/qwik/jsx-runtime';
 import styles from './button.css?inline';
 
 interface ButtonProps {
     text: string;
-    variant: 'primary' | 'secondary';
+    variant: 'primary' | 'secondary' | 'unstyled';
+    icon?: 'link';
     onClick?: () => void;
 }
 
 interface ButtonLinkProps {
     text: string;
-    variant: 'primary' | 'secondary';
+    variant: 'primary' | 'secondary' | 'unstyled';
     href: string;
     target: '_blank' | '_self'
+    icon?: 'link';
 }
 
 
-export const Button = component$(({ text, variant, ...rest }: ButtonProps | ButtonLinkProps) => {
+export const Button = component$(({ text, variant, icon, ...rest }: ButtonProps | ButtonLinkProps) => {
     useStylesScoped$(styles);
 
     function getButtonClassname(variant: ButtonProps['variant']): string {
@@ -25,15 +28,29 @@ export const Button = component$(({ text, variant, ...rest }: ButtonProps | Butt
                 return 'primary';
             case 'secondary':
                 return 'secondary';
+            case 'unstyled':
+                return 'unstyled';
         }
+    }
+
+    function getIcon(icon: ButtonProps['icon']): string | undefined {
+        switch (icon) {
+            case 'link':
+                return '/link-icon.png';
+            case undefined: return undefined
+        }
+    }
+
+    function getIconComponent(icon: ButtonProps['icon']): JSX.Element | null {
+        return icon ? <img src={getIcon(icon)} width="15px" height="15px" /> : null
     }
 
     if ((rest as LinkProps).href) {
         const { href, target } = (rest as LinkProps);
-        return <button class={getButtonClassname(variant)}><Link href={href} target={target} >{text}</Link></button>
+        return <button class={getButtonClassname(variant)}><Link href={href} target={target}>{text}{getIconComponent(icon)}</Link></button>
     } else {
         return (
-            <button class={getButtonClassname(variant)} onClick$={(rest as ButtonProps).onClick}>{text}</button>
+            <button class={getButtonClassname(variant)} onClick$={(rest as ButtonProps).onClick}>{text}{getIconComponent(icon)}</button>
         );
     }
 });
--- qwik-app/src/routes/cv/index.tsx
+++ qwik-app/src/routes/cv/index.tsx
@@ -1,5 +1,6 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import { DocumentHead } from "@builder.io/qwik-city";
+import { Button } from "~/components/button/button";
 import styles from "./index.css?inline";
 
 export const head: DocumentHead = {
@@ -9,8 +10,30 @@ export const head: DocumentHead = {
 export default component$(() => {
     useStylesScoped$(styles);
     return (
-        <>
-            hello card
-        </>
+        <div class="page-divider">
+            <h2>Work experience</h2>
+            <dl>
+                <dt><Button text="Famly" variant="unstyled" href="https://www.famly.co/de" target="_blank" icon="link" /></dt>
+                <dd>Early years education
+                    <dl>
+                        <dt>Senior frontend engineer</dt>
+                        <dd>Planning and implementation of architectural features and enhancements to the frontend.</dd>
+
+                        <dt>Frontend engineer II</dt>
+                        <dd>Planning and implementation of features on the Famly platform. Main focus: Staffing and attendance.</dd>
+                    </dl>
+                </dd>
+                <dt><Button text="Talkwalker" variant="unstyled" href="https://www.talkwalker.com/de" target="_blank" icon="link" /></dt>
+                <dd>Social media analtytics
+                    <dl>
+                        <dt>Feature team coordinator</dt>
+                        <dd>Workload balancing, reporting and management of a team of five developers.</dd>
+
+                        <dt>Software engineer</dt>
+                        <dd>Technical design and implementation of high availability services.</dd>
+                    </dl>
+                </dd>
+            </dl>
+        </div>
     );
 });
