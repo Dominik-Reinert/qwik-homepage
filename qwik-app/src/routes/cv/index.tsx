4550f6d41e727358862b87c3d347ab2cc2720fa1
Domi <dominik.reinert.merzig@gmail.com>
2023-02-05 12:06:07 +0100

    add dates

--- qwik-app/src/routes/cv/index.tsx
+++ qwik-app/src/routes/cv/index.tsx
@@ -16,20 +16,20 @@ export default component$(() => {
                 <dt><Button text="Famly" variant="unstyled" href="https://www.famly.co/de" target="_blank" icon="link" /></dt>
                 <dd>Early years education
                     <dl>
-                        <dt>Senior frontend engineer</dt>
+                        <dt>Senior frontend engineer (Jan. 2023 - Today)</dt>
                         <dd>Planning and implementation of architectural features and enhancements to the frontend.</dd>
 
-                        <dt>Frontend engineer II</dt>
+                        <dt>Frontend engineer II (Jan. 2022 - Jan. 2023)</dt>
                         <dd>Planning and implementation of features on the Famly platform. Main focus: Staffing and attendance.</dd>
                     </dl>
                 </dd>
                 <dt><Button text="Talkwalker" variant="unstyled" href="https://www.talkwalker.com/de" target="_blank" icon="link" /></dt>
                 <dd>Social media analtytics
                     <dl>
-                        <dt>Feature team coordinator</dt>
+                        <dt>Feature team coordinator (July 2019 - Jan. 2022)</dt>
                         <dd>Workload balancing, reporting and management of a team of five developers.</dd>
 
-                        <dt>Software engineer</dt>
+                        <dt>Software engineer (Dec. 2017 - Jan. 2022)</dt>
                         <dd>Technical design and implementation of high availability services.</dd>
                     </dl>
                 </dd>
